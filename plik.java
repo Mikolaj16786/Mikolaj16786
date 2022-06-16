01f03415afdfc3ca82ffff2950a9e1c480bbf4f1
Mikolaj <wwx8669@student.wsb.warszawa.pl>
2022-06-16 17:33:28 +0200

    commit 2

---
+++ plik.java
@@ -0,0 +1,28 @@
+public class Garnek {
+    int srednica;
+    int wysokosc;
+   String kolor;
+
+   public Garnek() {
+   }
+
+   public Garnek(int srednica, int wysokosc) {
+       this.srednica = srednica;
+       this.wysokosc = wysokosc;
+       this.kolor = kolor;
+   }
+
+   public String gotuj(){
+       return "Gotowanie w trakcie";
+   }
+   public String gotuj(boolean czyDodalismySol){
+       if (czyDodalismySol){
+           return "Gotowanie z solą";
+       } else {
+           return "Gotowanie bez soli";
+       }
+   }
+   public int zrocTemperatureWrzeniaWody() {
+       return 100;
+   }
+}
